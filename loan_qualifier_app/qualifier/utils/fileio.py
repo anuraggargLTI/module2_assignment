785ff4bcf69996f376c6b7b8f4e9071ce5871f5b
anuraggargLTI <anurag_garg@live.com>
2022-12-10 20:12:04 +0530

    updating fileio after moving the function save_qualifying_loans

--- loan_qualifier_app/qualifier/utils/fileio.py
+++ loan_qualifier_app/qualifier/utils/fileio.py
@@ -5,6 +5,8 @@ This contains a helper function for loading and saving CSV files.
 
 """
 import csv
+from pathlib import Path
+import questionary
 
 
 def load_csv(csvpath):
@@ -28,3 +30,30 @@ def load_csv(csvpath):
         for row in csvreader:
             data.append(row)
     return data
+"""
+helper function to save qualifying loan data to a csv file
+function takes user input for the file location and saves the data
+"""
+def save_qualifying_loans(qualifying_loans):
+    """Saves the qualifying loans to a CSV file.
+
+    Args:
+        qualifying_loans (list of lists): The qualifying bank loans.
+    """
+    # @TODO: Complete the usability dialog for savings the CSV Files.
+    # YOUR CODE HERE!
+    if(len(qualifying_loans)>0):
+        decision_to_save = questionary.text("do you want to save qualifying loans in a csv file - y/n?").ask()
+        if(decision_to_save=='y' or decision_to_save=='Y'):
+          location_to_save = questionary.text("please provide the file location").ask()
+         # if(Path(location_to_save).exists()):
+        csvpath = Path(location_to_save)
+        with open(csvpath,'w') as csvfile:
+                csvwriter = csv.writer(csvfile) 
+                header = ["Lender","Max Loan Amount","Max LTV","Max DTI","Min Credit Score","Interest Rate"]
+                csvwriter.writerow(header)
+                for row_items in qualifying_loans:
+                    csvwriter.writerow(row_items)
+          #else:
+          #  print("Sorry you have not specified the location correctly")
+ 
